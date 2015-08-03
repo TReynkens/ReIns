7dcb4f823d56e987e0a49206235a6879ee9044f7
tom.reynkens@hotmail.com <tomreynkens@hotmail.com>
2015-08-03 14:20:09 +0200

    Allow for limit in excess-loss premium estimates + remove separate ExcessSplice functions

--- R/RiskMeasures.R
+++ R/RiskMeasures.R
@@ -2,9 +2,8 @@
 
 
 
-# Premium for excess loss insurance with retention u 
-# using Hill estimates
-ExcessHill <- function(data, gamma, u, endpoint = Inf, warnings = TRUE, plot = TRUE, add = FALSE,
+# Integrated tail function using Hill estimates
+IntTailHill <- function(data, gamma, u, endpoint = Inf, warnings = TRUE, plot = TRUE, add = FALSE,
                     main="Estimates for premium of excess-loss insurance", ...) {
   
   # Check input arguments
@@ -51,8 +50,8 @@ ExcessHill <- function(data, gamma, u, endpoint = Inf, warnings = TRUE, plot = T
   
 }
 
-
-ExcessHill_single <- function(t, gamma, u, endpoint = Inf) {
+# Auxiliary function used in ExcessSplice
+IntTailHill_single <- function(t, gamma, u, endpoint = Inf) {
   
   # Premium
   if (is.finite(endpoint)) {
@@ -68,9 +67,8 @@ ExcessHill_single <- function(t, gamma, u, endpoint = Inf) {
 }
 
 
-# Premium for excess loss insurance with retention u 
-# using EPD estimates
-ExcessEPD <- function(data, gamma, delta, tau, u, warnings = TRUE, plot = TRUE, add = FALSE,
+# Integrated tail function using EPD estimates
+IntTailEPD <- function(data, gamma, delta, tau, u, warnings = TRUE, plot = TRUE, add = FALSE,
                     main="Estimates for premium of excess-loss insurance", ...) {
   
   # Check input arguments
@@ -113,9 +111,8 @@ ExcessEPD <- function(data, gamma, delta, tau, u, warnings = TRUE, plot = TRUE, 
 
 
 
-# Premium for excess loss insurance with retention u 
-# using GPD-MLE estimates
-ExcessGPD <- function(data, gamma, sigma, u, warnings = TRUE, plot = TRUE, add = FALSE,
+# Integrated tail function using GPD-MLE estimates
+IntTailGPD <- function(data, gamma, sigma, u, warnings = TRUE, plot = TRUE, add = FALSE,
                       main="Estimates for premium of excess-loss insurance", ...) {
   
   # Check input arguments
@@ -155,43 +152,135 @@ ExcessGPD <- function(data, gamma, sigma, u, warnings = TRUE, plot = TRUE, add =
   
 }
 
-#######################################################
 
-ExcessSplice <- function(u, splicefit) {
+# Premium of excess-loss insurance with retentation M and limit L using Hill estimates
+ExcessHill <- function(data, gamma, M, L = Inf, endpoint = Inf, warnings = TRUE, plot = TRUE, add = FALSE,
+                        main="Estimates for premium of excess-loss insurance", ...) {
   
-  
-  type <- splicefit$type
-  
-  if(type[1]=="GPD") {
-    excess <- ExcessSpliceGPD(u=u, splicefit=splicefit)
-      
-  } else if(type[1] %in% c("Hill", "trHill")) {
-    excess <- ExcessSpliceHill(u=u, splicefit=splicefit)
-    
-  } else if(type[1] %in% c("cHill", "ciHill", "trciHill")) {
-    excess <- ExcessSplicecHill(u=u, splicefit=splicefit)
-    
-  } else {
-    stop("Invalid type.")
+  if (M<0) {
+    stop("M should be positive.")
   }
   
-  return(excess)
+  if (L<0) {
+    stop("L should be positive.")
+  }
+  
+  if (length(M)!=length(L)) {
+    if(length(M)!=1 & length(L)!=1) {
+      stop("M and L should have equal length or at least one of them should have length 1.")
+    }
+  }
+  
+  f <- function(u) IntTailHill(u=u, data=data, gamma=gamma, endpoint=endpoint, warnings=warnings, 
+                               plot=FALSE, add=FALSE) 
+  
+  res <- f(M)
+  Im <- res$premium
+  K <- res$k
+  
+  if(is.finite(L)) {
+    Il <- f(M+L)$premium
+  } else {
+    Il <- 0
+  }
+  
+  premium <- Im - Il
+  
+  
+  # plots if TRUE 
+  plotfun(K, premium[K], type="l", xlab="k", ylab="Premium", main=main, plot=plot, add=add, ...)
+  
+  output(list(k=K, premium=premium, M=M, L=L), plot=plot, add=add)
+  
 }
 
-# Premium for excess loss insurance with retention u 
-# using splicing of ME and (truncated) Pareto
-ExcessSpliceHill <- function(u, splicefit) {
-  
 
+# Premium of excess-loss insurance with retentation M and limit L using GPD-MLE estimates
+ExcessGPD <- function(data, gamma, sigma, M, L = Inf, warnings = TRUE, plot = TRUE, add = FALSE,
+                       main="Estimates for premium of excess-loss insurance", ...) {
+  
+  if (M<0) {
+    stop("M should be positive.")
+  }
+  
+  if (L<0) {
+    stop("L should be positive.")
+  }
+  
+  f <- function(u) IntTailGPD(u=u, data=data, gamma=gamma, sigma=sigma, warnings=warnings, 
+                               plot=FALSE, add=FALSE) 
+  
+  res <- f(M)
+  Im <- res$premium
+  K <- res$k
+  
+  if(is.finite(L)) {
+    Il <- f(M+L)$premium
+  } else {
+    Il <- 0
+  }
+  
+  premium <- Im - Il
+  
+  
+  # plots if TRUE 
+  plotfun(K, premium[K], type="l", xlab="k", ylab="Premium", main=main, plot=plot, add=add, ...)
+  
+  output(list(k=K, premium=premium, M=M, L=L), plot=plot, add=add)
+  
+}
+
+# Premium of excess-loss insurance with retentation M and limit L using EPD estimates
+ExcessEPD <- function(data, gamma, delta, tau, M, L = Inf, warnings = TRUE, plot = TRUE, add = FALSE,
+                      main="Estimates for premium of excess-loss insurance", ...) {
+  
+  
+  if (M<0) {
+    stop("M should be positive.")
+  }
+  
+  if (L<0) {
+    stop("L should be positive.")
+  }
+  
+  f <- function(u) IntTailEPD(u=u, data=data, gamma=gamma, delta=delta, tau=tau, warnings=warnings, 
+                              plot=FALSE, add=FALSE) 
+  
+  res <- f(M)
+  Im <- res$premium
+  K <- res$k
+  
+  if(is.finite(L)) {
+    Il <- f(M+L)$premium
+  } else {
+    Il <- 0
+  }
+  
+  premium <- Im - Il
+  
+  
+  # plots if TRUE 
+  plotfun(K, premium[K], type="l", xlab="k", ylab="Premium", main=main, plot=plot, add=add, ...)
+  
+  output(list(k=K, premium=premium, M=M, L=L), plot=plot, add=add)
+  
+}
+
+
+#######################################################
+
+# Integrated tail function of splicing of ME and (truncated) Pareto
+IntTailSpliceHill <- function(u, splicefit) {
+  
   MEfit <- splicefit$MEfit
   EVTfit <- splicefit$EVTfit
-
+  
   endpoint <- EVTfit$endpoint
   const <- splicefit$const
   l <- length(const)
   
   tvec <- splicefit$t
-
+  
   if(l>2) {
     stop("Splicing of ME with 3 or more Pareto distributions is not supported.")
   }
@@ -207,18 +296,18 @@ ExcessSpliceHill <- function(u, splicefit) {
         
         if (u[i] > tvec[2]) {
           # u[i]>t2 case
-          premium[i] <- (1-const[2]) * ExcessHill_single(t=tvec[2], gamma=EVTfit$gamma[2], endpoint=endpoint[2], u=u[i])
+          premium[i] <- (1-const[2]) * IntTailHill_single(t=tvec[2], gamma=EVTfit$gamma[2], endpoint=endpoint[2], u=u[i])
           
         } else if (u[i]<tvec[2] & u[i]>tvec[1]) {
           # t<u[i]<t2 case
           
           e <- min(tvec[2],endpoint[1])
           
-          par_t2 <- ExcessHill_single(t=tvec[1], gamma=EVTfit$gamma[1], endpoint=e, u=tvec[2])
-          par_u <- ExcessHill_single(t=tvec[1], gamma=EVTfit$gamma[1], endpoint=e, u=u[i])
+          par_t2 <- IntTailHill_single(t=tvec[1], gamma=EVTfit$gamma[1], endpoint=e, u=tvec[2])
+          par_u <- IntTailHill_single(t=tvec[1], gamma=EVTfit$gamma[1], endpoint=e, u=u[i])
           
           premium[i] <- (const[2]-const[1]) * (par_u - par_t2) + (1-const[2]) * (tvec[2]-u[i]) +  
-            (1-const[2]) * ExcessHill_single(t=tvec[2], gamma=EVTfit$gamma[2], endpoint=endpoint[2], u=tvec[2])
+            (1-const[2]) * IntTailHill_single(t=tvec[2], gamma=EVTfit$gamma[2], endpoint=endpoint[2], u=tvec[2])
           
         } else {
           # u<t case
@@ -231,13 +320,13 @@ ExcessSpliceHill <- function(u, splicefit) {
           Ft <- ME_cdf(tvec[1], shape = MEfit$shape, alpha = MEfit$beta, theta = MEfit$theta)
           
           e <- min(tvec[2],endpoint[1])
-          par_t <- ExcessHill_single(t=tvec[1], gamma=EVTfit$gamma[1], endpoint=e, u=tvec[1])
-          par_t2 <- ExcessHill_single(t=tvec[1], gamma=EVTfit$gamma[1], endpoint=e, u=tvec[2])
+          par_t <- IntTailHill_single(t=tvec[1], gamma=EVTfit$gamma[1], endpoint=e, u=tvec[1])
+          par_t2 <- IntTailHill_single(t=tvec[1], gamma=EVTfit$gamma[1], endpoint=e, u=tvec[2])
           
           premium[i] <- (me_u-me_t - (1-Ft) * (tvec[1]-u[i]))/Ft * const[1] + (1-const[1]) * (tvec[1]-u[i]) + 
             (const[2]-const[1]) * (par_t - par_t2) + 
             (1-const[2]) * (tvec[2]-tvec[1]) + 
-            (1-const[2]) * ExcessHill_single(t=tvec[2], gamma=EVTfit$gamma[2], endpoint=endpoint[2], u=tvec[2])
+            (1-const[2]) * IntTailHill_single(t=tvec[2], gamma=EVTfit$gamma[2], endpoint=endpoint[2], u=tvec[2])
         }
         
       } else {
@@ -250,7 +339,7 @@ ExcessSpliceHill <- function(u, splicefit) {
       
       if (u[i]>tvec[l] & u[i]<endpoint[l]) {
         # u[i]>t case
-        premium[i] <- (1-const[l]) * ExcessHill_single(t=tvec[l], gamma=EVTfit$gamma, endpoint=endpoint, u=u[i])
+        premium[i] <- (1-const[l]) * IntTailHill_single(t=tvec[l], gamma=EVTfit$gamma, endpoint=endpoint, u=u[i])
         
       } else if (u[i]<endpoint[l]) {
         # u[i]<t case
@@ -262,7 +351,7 @@ ExcessSpliceHill <- function(u, splicefit) {
         Ft <- ME_cdf(tvec[l], shape = MEfit$shape, alpha = MEfit$beta, theta = MEfit$theta)
         
         premium[i] <- (me_u-me_t - (1-Ft) * (tvec[l]-u[i]))/Ft * const[l] + (1-const[l]) * (tvec[l]-u[i]) + 
-          (1-const[l]) * ExcessHill_single(t=tvec[l], gamma=EVTfit$gamma, endpoint=endpoint[l], u=tvec[l])
+          (1-const[l]) * IntTailHill_single(t=tvec[l], gamma=EVTfit$gamma, endpoint=endpoint[l], u=tvec[l])
         
       } else {
         premium[i] <- 0
@@ -271,16 +360,14 @@ ExcessSpliceHill <- function(u, splicefit) {
     
   }
   
- 
+  
   return(premium)
 }
 
 
 
-
-# Premium for excess loss insurance with retention u 
-# using splicing of ME and (truncated) censored Pareto
-ExcessSplicecHill <- function(u, splicefit) {
+# Integrated tail function of splicing of ME and (truncated) censored Pareto
+IntTailSplicecHill <- function(u, splicefit) {
   
 
   MEfit <- splicefit$MEfit
@@ -295,7 +382,7 @@ ExcessSplicecHill <- function(u, splicefit) {
     
     if (u[i]>splicefit$t & u[i]<endpoint) {
       # u>t case
-      premium[i] <- (1-splicefit$const) * ExcessHill_single(t=splicefit$t, gamma=EVTfit$gamma, endpoint=endpoint, u=u[i])
+      premium[i] <- (1-splicefit$const) * IntTailHill_single(t=splicefit$t, gamma=EVTfit$gamma, endpoint=endpoint, u=u[i])
       
     } else if (u[i]<endpoint) {
       # u<t case
@@ -307,7 +394,7 @@ ExcessSplicecHill <- function(u, splicefit) {
       Ft <- ME_cdf(splicefit$t, shape = MEfit$shape, alpha = MEfit$beta, theta = MEfit$theta)
       
       premium[i] <- (me_u-me_t - (1-Ft) * (splicefit$t-u[i]))/Ft * splicefit$const + (1-splicefit$const) * (splicefit$t-u[i]) + 
-        (1-splicefit$const) * ExcessHill_single(t=splicefit$t, gamma=EVTfit$gamma, endpoint=endpoint, u=splicefit$t)
+        (1-splicefit$const) * IntTailHill_single(t=splicefit$t, gamma=EVTfit$gamma, endpoint=endpoint, u=splicefit$t)
       
     } else {
       premium[i] <- 0
@@ -317,9 +404,8 @@ ExcessSplicecHill <- function(u, splicefit) {
 }
 
 
-# Premium for excess loss insurance with retention u 
-# using splicing of ME and GPD
-ExcessSpliceGPD <- function(u, splicefit) {
+# Integrated tail function of splicing of ME and GPD
+IntTailSpliceGPD <- function(u, splicefit) {
   
   
 
@@ -353,6 +439,51 @@ ExcessSpliceGPD <- function(u, splicefit) {
 }
 
 
+# Premium of excess-loss insurance with retention M and limit L
+ExcessSplice <- function(M, L=Inf, splicefit) {
+  
+  if (M<0) {
+    stop("M should be positive.")
+  }
+  
+  if (L<0) {
+    stop("L should be positive.")
+  }
+  
+  
+  type <- splicefit$type
+  
+  if(type[1]=="GPD") {
+    f <- function(u) IntTailSpliceGPD(u, splicefit=splicefit)
+    
+  } else if(type[1] %in% c("Hill", "trHill")) {
+    f <- function(u) IntTailSpliceHill(u, splicefit=splicefit)
+    
+  } else if(type[1] %in% c("cHill", "ciHill", "trciHill")) {
+    f <- function(u) IntTailSplicecHill(u, splicefit=splicefit)
+    
+  } else {
+    stop("Invalid type.")
+  }
+  
+  
+  Im <- f(M)
+  
+  if(is.finite(L)) {
+    Il <- f(M+L)
+  } else {
+    Il <- 0
+  }
+  
+  premium <- Im - Il
+  
+  return(premium)
+  
+}
+
+
+
+#########################################################
 
 # Value-at-risk
 VaR <- function(p, splicefit) {
