8c04ae7ff04d43991e08b45225b08dbdbb22b308
tom.reynkens@hotmail.com <tomreynkens@hotmail.com>
2015-11-06 14:24:56 +0100

    Extend Turnbull estimator for lower censored data by adding truncation bounds

--- R/KaplanMeier.R
+++ R/KaplanMeier.R
@@ -106,7 +106,7 @@ KaplanMeier <- function(x, data, censored) {
 # censored is a vector which is 1 if a data point is censored and 0 otherwise,
 # giving censored=0 results in the ordinary sample CDF
 #
-Turnbull <- function(x, L, R, censored, conf.type = "plain", conf.int = 0.95) {
+Turnbull <- function(x, L, R, censored, trunclower = 0, truncupper = Inf, conf.type = "plain", conf.int = 0.95) {
   
   
   # Check input arguments
@@ -119,12 +119,38 @@ Turnbull <- function(x, L, R, censored, conf.type = "plain", conf.int = 0.95) {
     stop("R should be a numeric vector.")
   }
   
+  if (!is.numeric(trunclower)) {
+    stop("trunclower should be numeric.")
+  }
   
-  # Check input arguments
+  if (!is.numeric(truncupper)) {
+    stop("truncupper should be numeric.")
+  }
+  
+  # Check lengths
   if (length(L)!=length(R)) {
     stop("L and R should have the same length.")
   }
   
+  if (length(trunclower)!=1) {
+    stop("trunclower should have length 1.")
+  }
+  
+  if (length(truncupper)!=1) {
+    stop("truncupper should have length 1.")
+  }
+  
+  if (any(L<trunclower)) {
+    stop("All elements of L should be larger than trunclower.")
+  }
+  
+  if (any(R>truncupper)) {
+    stop("All elements of R should be smaller than truncupper.")
+  }
+  
+  if (any(L>R)) {
+    stop("Each element of L should be smaller or equal than each corresponding element of R.")
+  }
   
   if (!is.numeric(x)) {
     stop("x should be numeric.")
@@ -132,7 +158,8 @@ Turnbull <- function(x, L, R, censored, conf.type = "plain", conf.int = 0.95) {
   censored <- .checkCensored(censored, length(L))
   
   
-  ft <- .Turnbull_internal(L=L, R=R, censored=censored, conf.type = conf.type, conf.int = conf.int)
+  ft <- .Turnbull_internal(L=L, R=R, censored=censored, conf.type = conf.type, conf.int = conf.int,
+                           trunclower=trunclower, truncupper=truncupper)
   est <- ft$f(x)
   
   # Turnbull estimator for the CDF
@@ -141,16 +168,18 @@ Turnbull <- function(x, L, R, censored, conf.type = "plain", conf.int = 0.95) {
 
 
 # Function to return Turnbull stepfunction for survival function
-# No left censoring included!
-.Turnbull_internal <- function(L, R, censored, conf.type = "plain", conf.int = 0.95) {
+.Turnbull_internal <- function(L, R, censored, trunclower = 0, truncupper = Inf, 
+                               conf.type = "plain", conf.int = 0.95) {
   
-  event <- !censored
-  # 3 for interval censoring and 0 for right censoring
-  event[event==0 & is.finite(R)] <- 3
-  
-  type <- "interval"
+  # Uncensored observations
+  R[!censored] <- L[!censored]
+  # Right censored observations, set upper bound to NA
+  R[censored & R==truncupper] <- NA
+  # Left censored observations, set lower bound to NA
+  L[censored & L==trunclower] <- NA
 
-  fit  <- survfit(Surv(time=L, time2=R, event=event, type=type) ~1, 
+
+  fit  <- survfit(Surv(time=L, time2=R, type="interval2") ~1,
                   conf.type = conf.type, conf.int = conf.int)
 
   
@@ -158,27 +187,3 @@ Turnbull <- function(x, L, R, censored, conf.type = "plain", conf.int = 0.95) {
               
   return(list(f=f, fit=fit))
 }
-
-
-# # Sort the data with index return
-# s <- sort(Z, index.return = TRUE)
-# L <- s$x
-# sortix <- s$ix
-# R <- I[sortix]
-# 
-# 
-# # Turnbull estimator
-# event <- !censored[sortix]
-# # 3 for interval censoring and 0 for right censoring
-# event[event==0 & is.finite(R)] <- 3
-# type <- ifelse(all(is.finite(R)), "interval", "right")
-# 
-# if (type=="interval") {
-#   fit  <- survfit(Surv(time=L, time2=R, event=event, type="interval") ~1, conf.type="plain", conf.int=1-alpha)
-# } else {
-#   fit  <- survfit(Surv(time=L, event=event, type="right") ~1, conf.type="plain", conf.int=1-alpha)
-# }
-# 
-# f = stepfun(fit$time,c(1,fit$surv))
-# est <- f(x)
-
