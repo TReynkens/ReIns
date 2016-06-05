76ed9e3786611e89b1cbbad1bb971149e01d1088
TReynkens <tomreynkens@hotmail.com>
2016-06-05 16:57:22 +0200

    Update documentation of regression estimators

--- R/Regression.R
+++ R/Regression.R
@@ -1,84 +1,18 @@
 
-#Estimator of small tail probability 1-F_i(x) in regression case where gamma is constant
-#A are the estimates for A(i/n) for k = 1, ...,n-1
-ProbReg <- function(Z, A, q, plot = FALSE, add = FALSE, 
-                     main = "Estimates of small exceedance probability", ...) {
-  # Check input arguments
-  .checkInput(Z,scale=A)
-  
-  if (length(q)>1) {
-    stop("q should be a numeric of length 1.")
-  }
-  
-  
-  Zsort <- as.numeric(sort(Z))
-  n <- length(Z)
-  prob <- numeric(n)
-  K <- 1:(n-1)
-  
-  # Hill estimator for gamma
-  hill <- Hill(Z,plot=FALSE)$gamma
-  
-  # Estimator for small exceedance probability
-  prob[K] <- A * (K+1)/(n+1) * (q/Zsort[n-K])^(-1/hill[K])
-  prob[prob<0 | prob>1] <- NA
-  
-  # plots if TRUE
-  .plotfun(K, prob[K], type="l", xlab="k", ylab="1-F(x)", main=main, plot=plot, add=add, ...)
-  
-  
-  # output list with values of k, corresponding return period estimates 
-  # and the considered large quantile q
-  
-  .output(list(k=K, P=prob[K], q=q),plot=plot,add=add)
-}
 
-
-#Estimator of large quantile Q_i(1-p) in regression case where gamma is constant
-#A are the estimates for A(i/n) for k = 1, ...,n-1
-QuantReg <- function(Z, A, p, plot = FALSE, add = FALSE, 
-                       main = "Estimates of extreme quantile", ...) {
-  
-  # Check input arguments
-  .checkInput(Z,scale=A)
-  
-  .checkProb(p)
-  
-  Zsort <- as.numeric(sort(Z))
-  n <- length(Z)
-  Q <- numeric(n)
-  K <- 1:(n-1)
-  
-  # Hill estimator for gamma
-  hill <- Hill(Z,plot=FALSE)$gamma
-  
-  # Estimator for quantiles
-  Q[K] <- A^hill[K] * Zsort[n-K] * ((K+1)/((n+1)*p))^(hill[K])
-  
-  # plots if TRUE
-  .plotfun(K, Q[K], type="l", xlab="k", ylab="Q(1-p)", main=main, plot=plot, add=add, ...)
-  
-  # output list with values of k, corresponding quantile estimates 
-  # and the considered small tail probability p
-  
-  .output(list(k=K, Q=Q[K], p=p),plot=plot,add=add)
-}
-
-
-
-#Regression on scale parameter with constant gamma (estimator for A(s))
+# Regression on scale parameter with constant gamma (estimator for A(s))
 #
-#s is the point in which the estimated scale function is evaluated
+# s is the point in which the estimated scale function is evaluated
 #
-#Z is the response variable data
+# Z is the response variable data
 #
-#kernel is the kernel function used and h is the bandwidth
-#If h is equal to NULL, a bandwidth will be selected (similar to density).
+# kernel is the kernel function used and h is the bandwidth
+# If h is equal to NULL, a bandwidth will be selected (similar to density).
 
 ScaleReg <- function(s, Z, kernel =  c("gaussian", "epanechnikov", "rectangular",
-                                   "triangular", "biweight", "cosine", "optcosine"), 
-                 h = NULL, plot = TRUE, add = FALSE, 
-                 main = "Estimates of scale parameter", ...) {
+                                       "triangular", "biweight", "cosine", "optcosine"), 
+                     h = NULL, plot = TRUE, add = FALSE, 
+                     main = "Estimates of scale parameter", ...) {
   
   # Check input arguments
   .checkInput(Z)
@@ -90,7 +24,7 @@ ScaleReg <- function(s, Z, kernel =  c("gaussian", "epanechnikov", "rectangular"
   
   # Select right kernel function with bandwidth h (K_h(x) = K(x/h)/h)
   kernel <- match.arg(kernel)
-  kernelh <- .kernel_aux(kernel=kernel,h=h)
+  kernelh <- .kernel_aux(kernel=kernel, h=h)
   
   # Scale estimates in the point s for several values of k
   A <- numeric(n)
@@ -105,73 +39,76 @@ ScaleReg <- function(s, Z, kernel =  c("gaussian", "epanechnikov", "rectangular"
   .output(list(k=K,A=A[K]), plot=plot, add=add)
 }
 
-#Estimator for A_{1-F_i}(s)
-#
-#s is the point in which the estimated scale function is evaluated
-#
-#Z is the response variable data
-#
-#kernel is the kernel function used and h is the bandwidth
-#If h is equal to NULL, a bandwidth will be selected (similar to density).
-
-.ScaleReg_survi <- function(s, Z, kernel =  c("gaussian", "epanechnikov", "rectangular",
-                                            "triangular", "biweight", "cosine", "optcosine"), 
-                          h = NULL, plot = TRUE, add = FALSE, 
-                          main = "Estimates of scale parameter", ...) {
+# Estimator of small tail probability 1-F_i(x) in regression case where gamma is constant
+# A are the estimates for A(i/n) for k = 1, ...,n-1
+ProbReg <- function(Z, A, q, plot = FALSE, add = FALSE, 
+                    main = "Estimates of small exceedance probability", ...) {
   
   # Check input arguments
-  .checkInput(Z)
+  .checkInput(Z, scale=A)
   
-  # Sort data
+  if (length(q)>1) {
+    stop("q should be a numeric of length 1.")
+  }
+  
+  
   Zsort <- as.numeric(sort(Z))
   n <- length(Z)
+  prob <- numeric(n)
   K <- 1:(n-1)
   
   # Hill estimator for gamma
-  hill <- Hill(Z)$gamma
+  hill <- Hill(Z, plot=FALSE)$gamma
   
-  # Select right kernel function with bandwidth h (K_h(x) = K(x/h)/h)
-  kernel <- match.arg(kernel)
-  kernelh <- .kernel_aux(kernel=kernel,h=h)
-  
-  # Scale estimates in the point s for several values of k
-  A <- numeric(n)
-  
-  for(k in K) {
-    A[k] <- 1/(n+1) * sum( (Z>Zsort[n-k]) * Z[n-k]^(1/hill[k]) * kernelh( s-(1:n)/n ) )
-  }
+  # Estimator for small exceedance probability
+  prob[K] <- A[K] * (K+1)/(n+1) * (q/Zsort[n-K])^(-1/hill[K])
+  prob[prob<0 | prob>1] <- NA
   
   # plots if TRUE
-  .plotfun(K, A[K], type="l", xlab="k", ylab="Scale", main=main, plot=plot, add=add, ...)
+  .plotfun(K, prob[K], type="l", xlab="k", ylab="1-F(x)", main=main, plot=plot, add=add, ...)
   
-  .output(list(k=K,A=A[K]), plot=plot, add=add)
   
+  # output list with values of k, corresponding return period estimates 
+  # and the considered large quantile q
+  
+  .output(list(k=K, P=prob[K], q=q),plot=plot,add=add)
 }
 
 
-#Estimator for A_{1-F}
-#
-#Z is the response variable data
-
-.ScaleReg_surv = function(Z, plot = TRUE, add = FALSE, main = "Estimates of scale parameter", ...) {
+# Estimator of large quantile Q_i(1-p) in regression case where gamma is constant
+# A are the estimates for A(i/n) for k = 1, ...,n-1
+QuantReg <- function(Z, A, p, plot = FALSE, add = FALSE, 
+                     main = "Estimates of extreme quantile", ...) {
   
-  # Scale estimate of the response variable
-  s <- Scale(Z,plot=FALSE)
+  # Check input arguments
+  .checkInput(Z, scale=A)
   
-  K <- s$k
-  A <- c(s$A,0)
+  .checkProb(p)
+  
+  Zsort <- as.numeric(sort(Z))
+  n <- length(Z)
+  Q <- numeric(n)
+  K <- 1:(n-1)
+  
+  # Hill estimator for gamma
+  hill <- Hill(Z, plot=FALSE)$gamma
+  
+  # Estimator for quantiles
+  Q[K] <- Zsort[n-K] * ((K+1)/((n+1)*p) * A[K])^(hill[K])
   
   # plots if TRUE
-  .plotfun(K, A[K], type="l", xlab="k", ylab="gamma", main=main, plot=plot, add=add, ...)
+  .plotfun(K, Q[K], type="l", xlab="k", ylab="Q(1-p)", main=main, plot=plot, add=add, ...)
   
-  .output(list(k=K, A=A[K]), plot=plot, add=add)
+  # output list with values of k, corresponding quantile estimates 
+  # and the considered small tail probability p
   
+  .output(list(k=K, Q=Q[K], p=p),plot=plot,add=add)
 }
 
 
-#Auxiliary function to select kernel and returns K_h(x) = kernel(x/h)/h
-#h is the chosen bandwidth and if equal to NULL, a bandwidth will be selected
-#(similar to density)
+# Auxiliary function to select kernel and returns K_h(x) = kernel(x/h)/h
+# h is the chosen bandwidth and if equal to NULL, a bandwidth will be selected
+# (similar to density)
 .kernel_aux <- function(kernel =  c("gaussian", "epanechnikov", "rectangular",
                                    "triangular", "biweight", "cosine", "optcosine"), h = NULL) {
   
@@ -180,13 +117,13 @@ ScaleReg <- function(s, Z, kernel =  c("gaussian", "epanechnikov", "rectangular"
   # Set default bandwidth
   if(is.null(h) | !is.numeric(h)) {
     bw <- switch(kernel,
-                  gaussian = 1/(2*sqrt(pi)),
-                  rectangular = sqrt(3)/6,
-                  triangular  = sqrt(6)/9,
-                  epanechnikov= 3/(5*sqrt(5)),
-                  biweight    = 5*sqrt(7)/49,
-                  cosine      = 3/4*sqrt(1/3 - 2/pi^2),
-                  optcosine   = sqrt(1-8/pi^2)*pi^2/16
+                 gaussian = 1/(2*sqrt(pi)),
+                 rectangular = sqrt(3)/6,
+                 triangular  = sqrt(6)/9,
+                 epanechnikov= 3/(5*sqrt(5)),
+                 biweight    = 5*sqrt(7)/49,
+                 cosine      = 3/4*sqrt(1/3 - 2/pi^2),
+                 optcosine   = sqrt(1-8/pi^2)*pi^2/16
     )
   } else {
     bw <- h
