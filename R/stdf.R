423b9eb631df42043c9d60fdf98379e4c046fab9
tom.reynkens@hotmail.com <tomreynkens@hotmail.com>
2016-11-22 18:59:49 +0100

    Rename cpp functions for stdf

--- R/stdf.R
+++ R/stdf.R
@@ -6,11 +6,11 @@ stdf <- function(x, k, X, alpha = 0.5) {
 
   # Convert to rank matrix
   R <- apply(as.matrix(X), 2, rank)
-  return(.Call("ReIns_stdf", as.numeric(x), k, as.matrix(R), alpha))
+  return(.Call("ReIns_stdf_cpp", as.numeric(x), k, as.matrix(R), alpha))
 }
 
 # Call C++ and make sure the right input types are used
 stdf2 <- function(x, k, X) {
 
-  return(.Call("ReIns_stdf2", as.numeric(x), k, as.matrix(X)))
+  return(.Call("ReIns_stdf2_cpp", as.numeric(x), k, as.matrix(X)))
 }
