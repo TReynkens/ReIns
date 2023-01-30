b57af9a328f900a3948188d96dc748815435a37c
Tom Reynkens <tomreynkens@hotmail.com>
2023-01-30 08:20:15 +0100

    Change C++ standard from C++11 to C++17 (default for R 4.3.0)

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // stdf_cpp
 double stdf_cpp(const NumericVector& x, const int k, const NumericMatrix& R, const double alpha);
 RcppExport SEXP _ReIns_stdf_cpp(SEXP xSEXP, SEXP kSEXP, SEXP RSEXP, SEXP alphaSEXP) {
